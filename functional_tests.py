84a1fef38c0a16d8a481c90b276d3c7f73e8aa51
matthewdahmer <matthew.dahmer@gmail.com>
2015-03-25 17:25:34 -0400

    Refactor functional test

--- functional_tests.py
+++ functional_tests.py
@@ -15,6 +15,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Zac wants to check out this new to-do list online
         self.browser.get('http://localhost:8000')
@@ -36,14 +41,18 @@ class NewVisitorTest(unittest.TestCase):
         # When he hits enter/return, the page updates and now it lists
         # "1: clean up my room" as an item in the to-do list table
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: clean up my room', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: clean up my room')
 
         # There is still a text box to enter additional list items, which is good because there
         # are many other things Zac needs to do. He enters 'learn to tie my shoes'.
-        self.assertIn('2: learn to tie my shoes', [row.text for row in rows])
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('learn to tie my shoes')
+        inputbox.send_keys(Keys.ENTER)
+
+        # The page updates again and shows both items on his list
+        self.check_for_row_in_list_table('1: clean up my room')
+        self.check_for_row_in_list_table('2: learn to tie my shoes')
+
 
         self.fail('Finish the test!')
 
